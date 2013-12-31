94de4ea73d1c73992e236cf9a29abac8ff50299c
benrobinsons5a <ben_robinson@s5a.com>
2013-12-31 13:00:18 -0500

    functor generative tests

--- src/test/scala/FunctorSpec.scala
+++ src/test/scala/FunctorSpec.scala
@@ -1,19 +1,35 @@
 package test.scala
 
-import org.scalacheck.Prop.forAll
 import org.scalacheck.Prop._
 import org.scalacheck.{Properties, Gen}
+import test.scala.Boxed
 
 
-class Boxed[A](val t: A) {
-  def fmap[A,B](f:A => B)(ab:Boxed[A]): (Boxed[A] => Boxed[B]) = ab => new Boxed(f(ab.t))
+case class Boxed[A](val t: A) {
+  def fmap[A,B](f:A => B)(ab: Boxed[A]): Boxed[B] = new Boxed(f(ab.t))
   def id[A](a:A): Boxed[A] = new Boxed[A](a)
+  override def toString(): String = t.toString
 }
 
 object FunctorSpec extends Properties("Functor laws"){
 
   def length(a: String): Int = a.size
-  val boxedF = new Boxed("hello")
-  val liftedLength = boxedF.fmap(length)(boxedF)
+  def toUpper(k: String): String = k.toUpperCase
+
+  val functorGen: Gen[(Boxed[String], String)] = for {
+    a <- Gen.alphaStr
+    } yield (Boxed[String](a), a)
+
+  property("Functor preserves structure of homomorphism") = forAll(functorGen) { (input: (Boxed[String], String)) =>
+    input match {
+      case(m, x) => m.fmap(length _ compose toUpper _)(m).t equals (m.fmap(length) _ compose m.fmap(toUpper) _)(m).t
+    }
+  }
+
+  property("Functor preserves identity morphism") = forAll(functorGen) { (input: (Boxed[String], String)) =>
+    input match {
+      case(m, x) => m.fmap((x:String) => x)(m).t equals m.id(x).t
+    }
+  }
 
 }
